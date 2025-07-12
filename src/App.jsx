21619544520ff4b4a2e2954dc8fc86114385bc95
ndurushima <ndu1994@gmail.com>
2025-07-12 13:52:43 -0500

    added the header and got some basic info in the info card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
-import './App.css'
+import './App.css';
+import Header from './components/Header.jsx';
+import InfoCard from './components/InfoCard.jsx';
+import ProjectCard from './components/ProjectCard.jsx';
 
 function App() {
 
   return (
     <>
-      <div>
-       <p></p>
+      <div className="app-container">
+        <Header />
+        <InfoCard />
+        <ProjectCard />
       </div>
     </>
   )
